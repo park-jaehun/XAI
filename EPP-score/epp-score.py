c299d61fe312d64392b4cf2590bd80a2e1b46d1f
park-jaehun <pjhun588@gmail.com>
2021-06-28 17:42:04 +0900

    1차 epp-score 구현

--- EPP-score/epp-score.py
+++ EPP-score/epp-score.py
@@ -7,6 +7,8 @@ print(i.getPlayerRating("Hank"))
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.metrics import roc_auc_score
 
 df = pd.read_csv("data/train.csv")
@@ -17,17 +19,15 @@ dummy_Sex = pd.get_dummies(df["Sex"],prefix="Sex")
 data = df[cols_to_keep].join(dummy_Pclass.loc[:,"Pclass_2":])
 data = data.join(dummy_Sex.loc[:,"Sex_male":])
 
-data["intercept"] = 1.0
+X = data[:,1:]
+y = data[:,1]
+
 print(data.head())
+clf = RandomForestClassifier(max_depth=2)
+clf.fit(X,y)
+
 #------------------------------------------
 
-# logistic regression
-train_cols = data.columns[1:]
-data=data.fillna(data["Age"].mean())
-logit = sm.Logit(data["Survived"], data[train_cols])
-
-result = logit.fit()
-print(np.exp(result.params))
 
 
 
